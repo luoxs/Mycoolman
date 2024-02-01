35dc05ba7356e19b2f071b75f2d7ef313b1e4f31
xsluo <xsluo@126.com>
2024-02-01 16:41:34 +0800

    设计密码框

--- app/src/main/java/cn/edu/mycoolman/MainActivity.java
+++ app/src/main/java/cn/edu/mycoolman/MainActivity.java
@@ -3,8 +3,36 @@ package cn.edu.mycoolman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView lbmode;
+    private TextView label1;
+    private TextView label2;
+    private TextView label3;
+    private ImageView img;
+
+    private TextView lbselect;
+    private TextView lbcurrent;
+    private TextView lbsetting;
+    private Button btadd;
+    private Button btdrop;
+
+    private Button bthigh;
+    private Button btmedium;
+    private Button btlow;
+    private Button bton;
+    private Button btoff;
+    private Button btpower;
+    private Button bttemp;
+    private Button btbattery;
+    private Button btturbo;
+
+    private byte bytepass1;
+    private byte bytepass2;
+    private byte bytepass3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
