7f50253660f881133612ca95fca6799a0ca7630b
xsluo <xsluo@126.com>
2024-02-01 10:25:45 +0800

    22#除湿可调风速

--- app/src/main/java/cn/edu/mycoolman/HomeActivity.java
+++ app/src/main/java/cn/edu/mycoolman/HomeActivity.java
@@ -2,13 +2,28 @@ package cn.edu.mycoolman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
+    private ImageButton btscanqr;
+    private ImageButton btbluetooth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btbluetooth = (ImageButton) findViewById(R.id.btBluetooth);
+        btbluetooth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(HomeActivity.this, ListDevice.class);
+                startActivity(intent);
+            }
+        });
     }
 }
