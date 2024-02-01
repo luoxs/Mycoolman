dc8e381bb9ec60e7c01b45b85c7a54f3ef3ce66a
xsluo <xsluo@126.com>
2024-02-01 14:48:07 +0800

    设计密码框

--- app/src/main/java/cn/edu/mycoolman/DataRead.java
+++ app/src/main/java/cn/edu/mycoolman/DataRead.java
@@ -3,24 +3,23 @@ package cn.edu.mycoolman;
 public class DataRead {
     private byte start;
     private byte power;
-    private byte tempSetting;
+    private byte tempcool;
     private byte tempReal;
-    private byte mode;
-    private byte wind;
+    private byte frozesetting;
+    private byte frozereal;
     private byte turbo;
-    private byte sleep;
+    private byte heat;
+    private byte battery;
     private byte unit;
-    private byte countdown;
-    private byte logo;
-    private byte atmosphere;
-    private byte red;
-    private byte green;
-    private byte blue;
-    private byte brightness;
-    private byte errcode;
-    private byte version;
-    private byte reserver1;
-    private byte reserver2;
+    private byte status;
+    private byte errorcode;
+    private byte vhigh;
+    private byte vlow;
+    private byte gc;
+    private byte tempheat;
+    private byte timer;
+    private byte code1;
+    private byte code2;
     private byte crcH;
     private byte crcL;
     private byte end;
@@ -28,98 +27,102 @@ public class DataRead {
 
     public void setData(byte[] data) {
         this.data = data;
+        this.start = data[0];
         this.power = data[1];
-        this.tempSetting = data[2];
+        this.tempcool = data[2];
         this.tempReal = data[3];
-        this.mode = data[4];
-        this.wind = data[5];
+        this.frozesetting = data[4];
+        this.frozereal = data[5];
         this.turbo = data[6];
-        this.sleep = data[7];
+        this.heat = data[7];
         this.unit = data[8];
-        this.countdown = data[9];
-        this.logo = data[10];
-        this.atmosphere = data[11];
-        this.red = data[12];
-        this.green = data[13];
-        this.blue = data[14];
-        this.brightness = data[15];
-        this.errcode = data[16];
+        this.battery = data[9];
+        this.unit = data[10];
+        this.status = data[11];
+        this.errorcode = data[12];
+        this.vhigh = data[13];
+        this.vlow = data[14];
+        this.gc = data[15];
+        this.tempheat = data[16];
+        this.timer = data[17];
+        this.code1 = data[18];
+        this.code2 = data[10];
+    }
+
+    public byte getStart() {
+        return start;
     }
 
     public byte getPower() {
         return power;
     }
 
-    public byte getTempSetting() {
-        return tempSetting;
+    public byte getTempcool() {
+        return tempcool;
     }
 
     public byte getTempReal() {
         return tempReal;
     }
 
-    public byte getMode() {
-        return mode;
+    public byte getFrozesetting() {
+        return frozesetting;
     }
 
-    public byte getWind() {
-        return wind;
+    public byte getFrozereal() {
+        return frozereal;
     }
 
     public byte getTurbo() {
         return turbo;
     }
 
-    public byte getSleep() {
-        return sleep;
+    public byte getHeat() {
+        return heat;
+    }
+
+    public byte getBattery() {
+        return battery;
     }
 
     public byte getUnit() {
         return unit;
     }
 
-    public byte getCountdown() {
-        return countdown;
+    public byte getStatus() {
+        return status;
     }
 
-    public byte getLogo() {
-        return logo;
+    public byte getErrorcode() {
+        return errorcode;
     }
 
-    public byte getAtmosphere() {
-        return atmosphere;
+    public byte getVhigh() {
+        return vhigh;
     }
 
-    public byte getRed() {
-        return red;
+    public byte getVlow() {
+        return vlow;
     }
 
-    public byte getGreen() {
-        return green;
+    public byte getGc() {
+        return gc;
     }
 
-    public byte getBlue() {
-        return blue;
+    public byte getTempheat() {
+        return tempheat;
     }
 
-    public byte getBrightness() {
-        return brightness;
+    public byte getTimer() {
+        return timer;
     }
 
-    public byte getErrcode() {
-        return errcode;
+    public byte getCode1() {
+        return code1;
     }
 
-    public byte getVersion() {
-        return version;
-    }
-
-    public byte getReserver1() {
-        return reserver1;
-    }
-
-    public byte getReserver2() {
-        return reserver2;
+    public byte getCode2() {
+        return code2;
     }
 
     public byte getCrcH() {
@@ -128,5 +131,13 @@ public class DataRead {
 
     public byte getCrcL() {
         return crcL;
+    }
+
+    public byte getEnd() {
+        return end;
+    }
+
+    public byte[] getData() {
+        return data;
     }
 }
