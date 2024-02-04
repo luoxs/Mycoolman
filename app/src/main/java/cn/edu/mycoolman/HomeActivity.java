d5da83425f51560b3af6e1dbf16dd839e81b6a6d
xsluo <xsluo@126.com>
2024-02-04 14:26:17 +0800

    返回功能

--- app/src/main/java/cn/edu/mycoolman/HomeActivity.java
+++ app/src/main/java/cn/edu/mycoolman/HomeActivity.java
@@ -2,10 +2,16 @@ package cn.edu.mycoolman;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 
 public class HomeActivity extends AppCompatActivity {
     private ImageButton btscanqr;
@@ -16,6 +22,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         btbluetooth = (ImageButton) findViewById(R.id.btBluetooth);
         btbluetooth.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +33,33 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        //扫描二维码
+        ImageButton btscanqr = findViewById(R.id.btscanqr);
+        btscanqr.setOnClickListener(view -> initScan());
+    }
+
+    public void initScan() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        // 设置要扫描的条码类型，ONE_D_CODE_TYPES：一维码，QR_CODE_TYPES-二维码
+        integrator.setDesiredBarcodeFormats();
+        integrator.setCaptureActivity(CaptureActivity.class); //设置打开摄像头的Activity
+        integrator.setPrompt("Please shoot sqrcode"); //底部的提示文字，设为""可以置空
+        integrator.setCameraId(0); //前置或者后置摄像头
+        integrator.setBeepEnabled(true); //扫描成功的「哔哔」声，默认开启
+        integrator.setBarcodeImageEnabled(true);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == IntentIntegrator.REQUEST_CODE) {
+            IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+            if (scanResult != null && scanResult.getContents() != null) {
+                String result = scanResult.getContents();
+                Log.d("扫码返回: ", result);
+                // tvMsg.setText(result);
+            }
+        }
     }
 }
